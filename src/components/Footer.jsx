babf060ab4649dda368cf0ad78d079806962a5ca
chegeMP <mmureithi@kabarak.ac.ke>
2024-03-21 23:35:03 +0300

    usercta has been added to the footer page

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,8 +1,11 @@
 import React from 'react';
+import UserCTA from './UserCTA';
 
 const Footer = () => {
   return (
     <footer className="bg-opacity-100 bg-white text-black p-4 text-center md:p-6 lg:p-8 xl:p-10">
+<UserCTA />
+
       <p className="text-sm md:text-base lg:text-lg xl:text-xl">
         &copy; {new Date().getFullYear()} <a 
         href="https://felix-jumason.netlify.app/"
---
+++ src/components/UserCTA.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function UserCTA() {
+  return (
+    <div>UserCTA</div>
+  )
+}
+
+export default UserCTA
